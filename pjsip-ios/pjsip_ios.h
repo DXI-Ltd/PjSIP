aa6f664592e9858b9d52118f511b2848cb5935ca
Otavio Zabaleta <otavio.zabaleta@dxi-easycall.net>
2015-05-13 10:38:01 +0100

    fixing

---
+++ pjsip-ios/pjsip_ios.h
@@ -0,0 +1,14 @@
+//
+//  pjsip_ios.h
+//  pjsip-ios
+//
+//  Created by Otavio Zabaleta on 13/05/2015.
+//  Copyright (c) 2015 DXI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DXIPJSipManager.h"
+
+@interface pjsip_ios : NSObject
+
+@end
---
+++ pjsip-ios/pjsip_ios.m
@@ -0,0 +1,13 @@
+//
+//  pjsip_ios.m
+//  pjsip-ios
+//
+//  Created by Otavio Zabaleta on 13/05/2015.
+//  Copyright (c) 2015 DXI. All rights reserved.
+//
+
+#import "pjsip_ios.h"
+
+@implementation pjsip_ios
+
+@end
