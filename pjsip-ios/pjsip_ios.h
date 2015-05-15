f5c26d476fe1671164776fa04d7474d1f6836200
Otavio Zabaleta <otavio.zabaleta@dxi-easycall.net>
2015-05-15 09:38:03 +0100

    final version?

--- pjsip-ios/pjsip_ios.h
+++ pjsip-ios/pjsip_ios.h
@@ -7,7 +7,9 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "DXIPJSipManager.h"
+#import <pjsua-lib/pjsua.h>
+#import <pjsua-lib/pjsua_internal.h>
+#import <pjsip/sip_msg.h>
 
 @interface pjsip_ios : NSObject
 
